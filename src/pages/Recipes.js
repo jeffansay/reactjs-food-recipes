4b3a45b6aa1743f8d6971f054b9cc0c2793a138c
unknown <jeff_ansay@ligph.com>
2019-07-17 17:43:50 +0800

    router is finished

---
+++ src/pages/Recipes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Recipes extends Component {
+    render() {
+        return (
+            <h4>
+                Hello from recipe pages
+            </h4>
+        );
+    }
+}
+
+export default Recipes;
